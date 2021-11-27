be8dbdf0e6f3850742300a74c8cf88b4b30801ac
jrmeman <42852542+jrmeman@users.noreply.github.com>
2021-11-26 17:49:22 -0800

    Update dbtest_JM.py

--- db-test/dbtest_JM.py
+++ db-test/dbtest_JM.py
@@ -1,35 +1,35 @@
 f = open('log.txt', 'r', errors='ignore')
 
-content = f.readline()
-content2 = content.split()
+for line in f:
+    content2 = line.split()
 
-sample = ''
-messageID = ''
-for char in content2[1]:
-    if char != ']':
-        sample += char
-messageID = sample[9:]
-print('MessageID: ' + messageID)
+    sample = ''
+    messageID = ''
+    for char in content2[1]:
+        if char != ']':
+            sample += char
+    messageID = sample[9:]
+    print('MessageID: ' + messageID)
 
-datetime = ''
-for item in content2[0:2]:
-    for char in item:
-        if char != '[' and char != ']':
-            datetime += char
-    datetime += ' '
-print('Date and time: ' + datetime)
+    datetime = ''
+    for item in content2[0:2]:
+        for char in item:
+            if char != '[' and char != ']':
+                datetime += char
+        datetime += ' '
+    print('Date and time: ' + datetime)
 
-user = ''
-for char in content2[2]:
-    if char != ':':
-        user += char
-print('User: ' + user)
+    user = ''
+    for char in content2[2]:
+        if char != ':':
+            user += char
+    print('User: ' + user)
 
-message = ''
-for item in content2[3:]:
-    for char in item:
-        message += char
-    message += ' '
-print('Message: ' + message)
+    message = ''
+    for item in content2[3:]:
+        for char in item:
+            message += char
+        message += ' '
+    print('Message: ' + message)
 
 f.close()
