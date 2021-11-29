528f5f60de2ae61ad5dcabcb5f437de8fcd9ac3e
jrmeman <42852542+jrmeman@users.noreply.github.com>
2021-11-28 16:46:00 -0800

    Update dbtest_JM.py

--- db-test/dbtest_JM.py
+++ db-test/dbtest_JM.py
@@ -1,37 +1,100 @@
-f = open('log.txt', 'r', errors='ignore')
+# Benchmark: 15 messages
 
-for line in f:
-    content2 = line.split()
+def find_repeats(arr, required_number, num_repeats):
+    idx = 0
+    idxArr = []
+    while idx < len(arr):
+        if [required_number]*num_repeats == arr[idx:idx+num_repeats]:
+            idxArr.append(idx)
+            idx += num_repeats
+        else:
+            idx += 1
+    return idxArr
 
-    sample = ''
-    messageID = ''
-    for char in content2[1]:
-        if char != ']':
-            sample += char
-    messageID = sample[9:]
-    print('MessageID: ' + messageID)
+def main():
+    f = open('log.txt', 'r', errors='ignore')
 
-    datetime = ''
-    for item in content2[0:2]:
-        for char in item:
-            if char != '[' and char != ']':
-                datetime += char
-        datetime += ' '
-    print('Date and time: ' + datetime)
+    timeArr = []
+    secArr = []
+    rateArr = []
+    count = 0
+    for line in f:
+        content = line.split()
 
-    user = ''
-    for char in content2[2]:
-        if char != ':':
-            user += char
-    print('User: ' + user)
+        sample = ''
+        messageID = ''
+        for char in content[1]:
+            if char != ']':
+                sample += char
+        messageID = sample[9:]
+        #print('MessageID: ' + messageID)
 
-    message = ''
-    for item in content2[3:]:
-        for char in item:
-            message += char
-        message += ' '
-    print('Message: ' + message)
+        datetime = ''
+        for item in content[0:2]:
+            for char in item:
+                if char != '[' and char != ']':
+                    datetime += char
+            datetime += ' '
+        #print('Date and time: ' + datetime)
+        timeArr.append(datetime)
 
-    print('\n')
+        user = ''
+        for char in content[2]:
+            if char != ':':
+                user += char
+        #print('User: ' + user)
 
-f.close()
+        message = ''
+        for item in content[3:]:
+            for char in item:
+                message += char
+            message += ' '
+        #print('Message: ' + message)
+
+        # Algorithm for finding rates and peak finder
+        time = ''
+        for item in content[1]:
+            for char in item:
+                if char != ']':
+                    time += char
+        hour = float(time[0:2])
+        # Convert hours to seconds
+        totalSec = hour * 60 * 60
+
+        min = float(time[3:5])
+        # Convert min to seconds
+        totalSec = totalSec + min * 60
+
+        sec = float(time[6:])
+        totalSec = totalSec + sec
+        secArr.append(totalSec)
+        count = count + 1
+
+        #print('\n')
+
+    secArr.sort()
+    time2 = 0.0
+    for i in range(1, count):
+        time2 = (secArr[i] - secArr[i-1]) / 2
+        rateArr.append(round(time2, 2))
+
+    boolArr = []
+    for i in range(len(rateArr)):
+        if rateArr[i] < 0.05:
+            boolArr.append(True)
+        else:
+            boolArr.append(False)
+
+    # Peak chat activity is based off of 10 consecutive chats that were sent consecutively
+    # in less than 0.05 seconds within each one
+    print("Timestamps of peak chat activity:")
+    for i in (find_repeats(boolArr, True, 40)):
+        print("Time Frame: " + timeArr[i] + "- " + timeArr[i+14])
+
+    totalTime = secArr[-1] - secArr[0]
+    totalRate = count / totalTime
+    print("Total rate: " + str(round(totalRate, 2)) + " messages/sec")
+
+    f.close()
+
+main()
